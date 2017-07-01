53d1bb428b5b50bca4d384492eee4f266825bf9b
Salvatore Campagna <salvatorecampagna@gmail.com>
2017-07-01 10:32:02 +0200

    Remove projects directory

--- projects/LeNet/README.txt
+++
@@ -1,4 +0,0 @@
-LeNet-5 is a convolutional network designed for handwritten and
-machine-printed character recognition.
-
-Please look at: http://yann.lecun.com/exdb/lenet/
--- projects/miniflow/gd.py
+++
@@ -1,8 +0,0 @@
-def gradient_descent_update(x, gradx, learning_rate):
-    """
-    Performs a gradient descent update.
-    """
-    x = x - learning_rate * gradx
-
-    # Return the new value for x
-    return x
