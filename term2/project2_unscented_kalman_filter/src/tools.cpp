9d271ce024fc3f63f7ba1d34956b70f76974704c
Salvatore Campagna <salvatorecampagna@gmail.com>
2017-07-15 11:04:13 +0200

    UKF: implement RSME calculation

--- term2/project2_unscented_kalman_filter/src/tools.cpp
+++ term2/project2_unscented_kalman_filter/src/tools.cpp
@@ -11,8 +11,32 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd rmse(4);
+	rmse << 0, 0, 0, 0;
+
+  if (estimations.size() == 0) {
+    std::cout << "CalculateRMSE - Error: Invalid estimation size" << std::endl;
+    return rmse;
+  }
+
+  if (ground_truth.size() == 0) {
+    std::cout << "CalculateRMSE - Error: Invalid groud truth size" << std::endl;
+    return rmse;
+  }
+
+  if (estimations.size() != ground_truth.size()) {
+    std::cout << "CalculateRMSE - Error: Size mismatch" << std::endl;
+    return rmse;
+  }
+
+  for (int i = 0; i < estimations.size(); ++i) {
+        VectorXd error = estimations[i] - ground_truth[i];
+        error = error.array() * error.array();
+        rmse += error;
+	}
+
+	rmse = rmse / estimations.size();
+	rmse = rmse.array().sqrt();
+
+	return rmse;
 }
