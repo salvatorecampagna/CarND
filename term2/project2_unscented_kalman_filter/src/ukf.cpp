94646ee767e97363b254f9ec7d8a460687861b57
Salvatore Campagna <salvatorecampagna@gmail.com>
2017-07-15 12:10:18 +0200

    UKF: initialize the UKF using the first measurement

--- term2/project2_unscented_kalman_filter/src/ukf.cpp
+++ term2/project2_unscented_kalman_filter/src/ukf.cpp
@@ -24,10 +24,12 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  //std_a_ = 30; //TODO: tune this value
+  std_a_ = 3.0;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  //std_yawdd_ = 30; //TODO: tune this value
+  std_yawdd_ = 2.0;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -44,13 +46,20 @@ UKF::UKF() {
   // Radar measurement noise standard deviation radius change in m/s
   std_radrd_ = 0.3;
 
-  /**
-  TODO:
+  // Number of state variables (size of x_)
+  n_x_ = 5;
 
-  Complete the initialization. See ukf.h for other member properties.
+  // Add two dimensions to the state vector (nu_a and nu_yawdd)
+  n_aug_ = n_x_ + 2;
 
-  Hint: one or more values initialized above might be wildly off...
-  */
+  // Lambda for mean and covariance calculation
+  lambda_ = 3 - n_x_;
+
+  // Predicted sigma points
+  Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);
+
+  // Weights for mean and covariance calculation
+  weights_ = VectorXd(2 * n_aug_ + 1);
 }
 
 UKF::~UKF() {}
@@ -60,12 +69,50 @@ UKF::~UKF() {}
  * either radar or laser.
  */
 void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
-  /**
-  TODO:
+  double rho, phi, rho_dot, px, py, v, yaw, yawd;
 
-  Complete this function! Make sure you switch between lidar and radar
-  measurements.
-  */
+  if (!is_initialized_)
+  {
+    x_ << 1.0, 1.0, 1.0, 1.0, 1.0;
+
+    if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+      //range
+      rho = meas_package.raw_measurements_[0];
+      //bearing
+      phi = meas_package.raw_measurements_[1];
+      //radial velocity
+      rho_dot = meas_package.raw_measurements_[2];
+      //convert polar to cartesian
+      px = rho * cos(phi);
+      py = rho * sin(phi);
+      v = 0.0;
+      yaw = 0.0;
+      yawd = 0.0;
+
+    } else if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
+      //position (x)
+      px = meas_package.raw_measurements_[0];
+      //position (y)
+      py = meas_package.raw_measurements_[1];
+      //laser does not provide speed measurements
+      v = 0.0;
+      yaw = 0.0;
+      yawd = 0.0;
+
+    }
+
+    //initialize state variable using the first measurement - radar
+    x_ << px, py, v, yaw, yawd;
+
+    //update time
+    time_us_ = meas_package.timestamp_;
+
+    //done initializing, no need to predict or update
+    is_initialized_ = true;
+    return;
+  }
+
+
 }
 
 /**
