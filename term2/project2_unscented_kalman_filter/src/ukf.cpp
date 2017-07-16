947646a097d33e3a2eb50a3eccaf2d645f512553
Salvatore Campagna <salvatorecampagna@gmail.com>
2017-07-16 13:49:10 +0200

    UKF: set finale values for process noise

--- term2/project2_unscented_kalman_filter/src/ukf.cpp
+++ term2/project2_unscented_kalman_filter/src/ukf.cpp
@@ -25,10 +25,10 @@ UKF::UKF() {
   P_ = MatrixXd::Identity(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 3.0;
+  std_a_ = 1.0;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 2.0;
+  std_yawdd_ = 0.5;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
